aa090f37dd69743f333e50fa1fb75263f295d0b3
sl <singsongforme.163.com>
2020-11-03 10:25:25 +0800

    Io test

---
+++ go-test/io/main/io.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	os.Mkdir("C:/Users/97556/Desktop/go", 1)
+	os.Create("C:/Users/97556/Desktop/go/1.txt")
+	file, _ := os.OpenFile("C:/Users/97556/Desktop/go/1.txt", os.O_APPEND, 777)
+	io.WriteString(file, "hello")
+
+	file.Close()
+}
