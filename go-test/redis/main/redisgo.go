8bd99756d34ae08e849126255d06fad967e475e6
sl <singsongforme.163.com>
2020-11-03 10:26:29 +0800

    interface test

--- go-test/redis/main/redisgo.go
+++ go-test/redis/main/redisgo.go
@@ -1,17 +1,19 @@
 package main
-import(
+
+import (
 	"fmt"
 	"github.com/garyburd/redigo/redis"
 )
-func main(){
-c,err := redis.Dial("tcp","127.0.0.1:6379")
-if err != nil{
-	fmt.Println(err)
-	return
-}
-defer c.Close()
-v,err := c.Do("SET","name","red")
-	if err != nil{
+
+func main() {
+	c, err := redis.Dial("tcp", "127.0.0.1:6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer c.Close()
+	v, err := c.Do("SET", "name", "red")
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
