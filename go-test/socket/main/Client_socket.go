3383df4c7996cbbecd6eaa74e96fdb0113a474a4
sl <singsongforme.163.com>
2020-11-03 10:27:26 +0800

    socket test

--- go-test/socket/main/Client_socket.go
+++ go-test/socket/main/Client_socket.go
@@ -5,28 +5,30 @@ import (
 	"net"
 	"time"
 )
+
 const RECV_BUF_LENC = 1024
-func checkc(err error ){
-	if err != nil{
-		panic("this err is:"+err.Error())
+
+func checkc(err error) {
+	if err != nil {
+		panic("this err is:" + err.Error())
 	}
 }
-func main(){
-	conn,err := net.Dial("tcp","0.0.0.0:6666")
+func main() {
+	conn, err := net.Dial("tcp", "0.0.0.0:6666")
 	checkc(err)
 	defer conn.Close()
-	buf :=make([]byte,RECV_BUF_LENC)
-	for i := 0;i < 5;i++{
+	buf := make([]byte, RECV_BUF_LENC)
+	for i := 0; i < 5; i++ {
 		msg := fmt.Sprintf("Hello World, %03d", i)
-		n,err := conn.Write([]byte(msg))
-		if err != nil{
+		n, err := conn.Write([]byte(msg))
+		if err != nil {
 			println("Write Buffer Error:", err.Error())
 			break
 		}
 		fmt.Println(msg)
 		//从服务器端收字符串
 		n, err = conn.Read(buf)
-		if err !=nil {
+		if err != nil {
 			println("Read Buffer Error:", err.Error())
 			break
 		}
---
+++ go-test/socket/main/Server_socket.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+const RECV_BUF_LEN = 1024
+
+func check(err error) {
+	if err != nil {
+		panic("this err is:" + err.Error())
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:6666") //监听6666端口
+	if err != nil {
+		panic("err listener:" + err.Error())
+	}
+	fmt.Println("start the server")
+	for {
+		conn, err := listener.Accept() //接受连接
+		check(err)
+		fmt.Println("Accepted the Connection", conn.RemoteAddr())
+		go EchoServer(conn)
+	}
+}
+func EchoServer(conn net.Conn) {
+	buf := make([]byte, RECV_BUF_LEN)
+	defer conn.Close()
+	for {
+		n, err := conn.Read(buf)
+		//check(err)
+		switch err {
+		case nil:
+			conn.Write(buf[0:n])
+		case io.EOF:
+			fmt.Printf("warning:end of data:%s\n", err)
+		default:
+			fmt.Printf("Error:Reading data:%s\n", err)
+			return
+
+		}
+	}
+}
