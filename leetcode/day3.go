2da7b13b159811f2872c803d1e6fa73603eca917
sl <singsongforme.163.com>
2020-12-30 16:00:16 +0800

    Mycode

---
+++ leetcode/day3.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+/**
+在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+示例:
+现有矩阵 matrix 如下：
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+给定 target=5，返回true。
+给定target=20，返回false。
+
+限制：
+0 <= n <= 1000
+0 <= m <= 1000
+
+链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof
+*/
+
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			if matrix[i][j] > target {
+				continue
+			}
+			if matrix[i][j] == target {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+	matrix := [][]int{
+		{1, 4, 7, 11, 15},
+		{2, 5, 8, 12, 19},
+		{3, 6, 9, 16, 22},
+		{10, 13, 14, 17, 24},
+		{18, 21, 23, 26, 30},
+	}
+
+	flag := findNumberIn2DArray(matrix, 5)
+	fmt.Println(flag)
+
+}
